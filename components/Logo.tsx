741250d20e8096ad91657b2d0b2467ebcbb3b444
eduvollet <eduardo.vollet.souza@gmail.com>
2025-08-28 16:06:03 -0300

    authentication-wallet-green-logo

---
+++ components/Logo.tsx
@@ -0,0 +1,17 @@
+import { Wallet } from "lucide-react"; // ícone substituto do saco de dinheiro
+import React from "react";
+
+function Logo() {
+    return (
+        <a href="/" className="flex items-center gap-2">
+            <Wallet className="h-11 w-11 stroke-green-600 stroke-[1.5]" />
+            <p className="bg-gradient-to-r from-green-500 to-green-700
+                bg-clip-text text-3xl font-bold leading-tight tracking-tighter
+                text-transparent">
+                FinanceManager
+            </p>
+        </a>
+    );
+}
+
+export default Logo;
